aad713f240002dc5ccbb32681f6f02357ea4c02f
Aaron Pius <piuschbz@gmail.com>
2023-09-05 21:04:03 +0000

    FizzBuzz

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10, 10)
+number = 98
 if number < 0:
     print(number, "is negative")
 elif number == 0:
     print(number, "is zero")
 else:
-    print(number, "is positve")
+    print(number, "is positive")
