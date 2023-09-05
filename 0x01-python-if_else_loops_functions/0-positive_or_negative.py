448ef49c5e0a853d975c4e8c26c2b9c0cb280dd8
Aaron Pius <piuschbz@gmail.com>
2023-09-05 17:32:31 +0000

    Numbers and strings manipulations

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number < 0:
-    print(number, "is negative")
+    print(number, "is negative\n")
+elif number == 0:
+    print(number, "is zero\n")
 else:
-    print(number, "is positve")
+    print(number, "is positve\n")
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-	digit = '-' + str(number)[-1]
+    digit = '-' + str(number)[-1]
 else:
-	digit = str(number)[-1]
+    digit = str(number)[-1]
 message = f"Last digit of {number} is {digit}"
 if int(digit) > 5:
-	print(message, "and is greater than 5")
+    print(message, "and is greater than 5")
 elif int(digit) != 0 and int(digit) < 6:
-	print(message, "and is less than 6 and not 0")
+    print(message, "and is less than 6 and not 0")
 else:
-	print(message, "and is 0")
+    print(message, "and is 0")
