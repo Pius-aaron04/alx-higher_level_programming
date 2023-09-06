6f663b3d4bc71754f4fac6472ce2158963ee75f3
Aaron Pius <piuschbz@gmail.com>
2023-09-06 11:09:16 +0000

    pycode styling

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(string, n):
     if n > len(string):
-        return(string);
+        return string
     new_string = ""
     for i, c in enumerate(string):
         if i == n:
