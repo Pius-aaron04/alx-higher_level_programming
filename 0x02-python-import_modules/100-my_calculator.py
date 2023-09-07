5a876dd2f2419542660ef78605d6726f9db4a2b1
Aaron Pius <piuschbz@gmail.com>
2023-09-07 20:43:40 +0000

    stdout instead for messages

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,11 +10,10 @@ operation = {
 }
 if __name__ == "__main__":
     if len(argv) != 3:
-        sys.stderr.write("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     if argv[1] not in '+-*/':
-        sys.stderr.write("Unknown operator. Available operators:" +
-                         "+, -, * and /\n")
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     a, op, b = argv
     print("{} {} {} = {}".format(a, op, b, operation[op](int(a), int(b))))
