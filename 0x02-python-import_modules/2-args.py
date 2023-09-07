dc6ed69c7484463605d5150c5b099289bdc34671
Aaron Pius <piuschbz@gmail.com>
2023-09-07 14:43:36 +0000

    Hidden discovery

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,10 @@ argv = sys.argv[1:]
 n = len(argv)
 if __name__ == "__main__":
     if n == 1:
-        print("1 argument")
+        print("1 argument:")
     elif n == 0:
         print("0 arguments.")
     else:
-        print("{} agruments:".format(n))
+        print("{} arguments:".format(n))
     for i, arg in enumerate(argv):
         print("{}: {}".format(i + 1, arg))
