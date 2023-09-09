2e128ccd4de322ba4d62090a9bebd56e5794fc2d
Aaron Pius <piuschbz@gmail.com>
2023-09-09 11:02:25 +0000

    Space complexity handled

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,57 +1,5 @@
-#include <stddef.h>
 #include <stdlib.h>
 #include "lists.h"
-#include <stdio.h>
-
-/**
- * _memcpy - copies memory block from a source to destination
- * memory block
- * @dest: destination memory
- * @src: source memory
- * @n: number of bytes
- * Return: dest
- */
-
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int k;
-
-	for (k = 0; k < n; k++)
-		dest[k] = src[k];
-	return (dest);
-}
-
-/**
- * _realloc - reallocates memory address
- * @ptr: pointer to be rallocated
- * @old_size: size to be changed
- * @new_size: size of memory to allocate
- * Return: void pointer
- */
-
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	void *new_memory;
-
-	if (new_size == old_size)
-		return (ptr);
-	if (ptr == NULL)
-	{
-		new_memory = malloc(new_size);
-		return (new_memory);
-	}
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	new_memory = malloc(new_size);
-	if (new_memory == NULL)
-		return (NULL);
-	_memcpy(new_memory, ptr, old_size);
-	free(ptr);
-	return (new_memory);
-}
 
 /**
  * is_palindrome - checks if a linked list is a palindrome
@@ -67,7 +15,7 @@ int is_palindrome(listint_t **head)
 	if (*head == NULL)
 		return (1);
 	if ((*head)->next == NULL)
-		return (0);
+		return (1);
 
 	/* transverse list and stores data in an array of numbers */
 	while (current)
@@ -87,7 +35,6 @@ int is_palindrome(listint_t **head)
 	i--;
 	for (; current; current = current->next, i--)
 	{
-		
 		if ((int)numbers[i] != current->n)
 			return (0);
 	}
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max_num = my_list[0]
+    if my_list == []:
+        return None
     for num in my_list:
+        # checks if current number is greater than max_num
         if num > max_num:
             max_num = num
     return max_num
