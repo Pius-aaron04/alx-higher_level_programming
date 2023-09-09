222900ec6a56df613602ddf3d6016dd0bc9f9f31
Aaron Pius <piuschbz@gmail.com>
2023-09-09 09:05:48 +0000

    --Index range adjusted--

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     my_list = my_list.copy()
-    if idx > len(my_list) or idx < 0:
+    if idx >= len(my_list) or idx < 0:
         return my_list
     my_list[idx] = element
     return my_list
