252cc08382e12275b613eacb54465915b3bcb26f
Aaron Pius <piuschbz@gmail.com>
2023-09-25 12:32:44 +0000

    Task 0 - 2

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        n = 0
+        for i in range(x):
+            print(my_list[i], end='')
+            n += 1
+        print()
+    except IndexError:
+        print()
+        return n
+    else:
+        return n
