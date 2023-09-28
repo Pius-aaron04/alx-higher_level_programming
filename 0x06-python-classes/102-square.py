92c0d7b1e31f3ef351b03a69b34599b5bc1d42e0
Aaron Pius <piuschbz@gmail.com>
2023-09-28 01:31:26 +0000

    Task 9

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+""" This module contains the definition of a Square Class with size."""
+
+
+class Square:
+    """ This is an empty Square Class."""
+    def __init__(self, size=0):
+        """ Initializes private size attribute for Square ininstance."""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must >= 0")
+        self.__size = size
+
+    def area(self):
+        """ Calculates the area of the Square instance."""
+
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """ gets the size attribue of the instance"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """ Sets or updates the size attribute """
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must >= 0")
+        self.__size = size
+
+    def __eq__(self, other):
+        """Does th equal comaparison"""
+        return self.size == other.size
+
+    def __lt__(self, other):
+        """Does the less than comaparison"""
+        return self.size < other.size
+
+    def __le__(self, other):
+        """Does the less than or equal comaparison"""
+        return self.size <= other.size
+
+    def __gt__(self, other):
+        """Does the greater than comaparison"""
+        return self.size > other.size
+
+    def __ge__(self, other):
+        """Does the great than and equal to"""
+        return self.size>= other.size
