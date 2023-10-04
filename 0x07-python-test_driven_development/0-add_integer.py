5678a588f4a4b1082a257dd983d9a58842191b17
Aaron Pius <piuschbz@gmail.com>
2023-10-04 09:57:28 +0000

    Corrections

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,12 +9,15 @@ def add_integer(a, b=98):
     this function adds two numbers.
     """
 
-    if type(a) not in (int, float):
+    if a is None or type(a) not in (int, float):
         raise TypeError('a must be an integer')
-    elif type(b) not in (int, float):
+    elif b is None or type(b) not in (int, float):
         raise TypeError('b must be an integer')
-    if type(a) is float:
-        a = int(a)
-    elif type(b) is float:
-        b = int(b)
-    return int(a) + int(b)
+    if a != a:
+        a = 89
+    if b != b:
+        b = 89
+    result = a + b
+    if result = float('inf') or result == -float('inf'):
+        return 89
+    return result
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -13,6 +13,6 @@ def say_my_name(first_name, last_name=""):
     elif type(last_name) is not str:
         raise TypeError("last_name must be a string")
     if len(first_name) == 0:
-        print(last_name)
+        print('My name is', last_name)
     else:
-        print(first_name, last_name)
+        print('My name is', first_name, last_name)
