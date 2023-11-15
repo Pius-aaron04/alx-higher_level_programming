45bc79fb8564937079cde246715fa9b99a9bbff6
Aaron Pius <piuschbz@gmail.com>
2023-11-15 13:02:02 +0000

    Task 6 to 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,14 @@
+-- Full table creation
+	CREATE TABLE IF NOT EXISTS second_table (
+		id INT,
+		name VARCHAR(256),
+		score INT
+	);
+
+	-- insert sample rows
+	INSERT INTO second_table
+	VALUES
+	(1, 'Jonh', 10),
+	(2, 'Alex', 3),
+	(3, 'Alex', 14),
+	(4, 'Alex', 8);
