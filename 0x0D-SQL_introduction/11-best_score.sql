d664ad4a1c1c4db268f1832a5cf5f664785b54ec
Aaron Pius <piuschbz@gmail.com>
2023-11-15 14:13:42 +0000

    Almost done with mandatory tasks

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,5 @@
 -- displays records with score greater than or equal 10
 SELECT score, name
 FROM second_table
-WHERE score >= 10;
+WHERE score >= 10
+ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,4 @@
+-- Lists records with name
+SELECT score, name
+FROM second_table
+WHERE name IS NOT NULL;
