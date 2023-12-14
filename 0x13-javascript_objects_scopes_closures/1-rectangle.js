f1fff3856adf3c071c44df9beb1e55a77099056d
Aaron Pius <piuschbz@gmail.com>
2023-12-14 02:34:16 +0000

    add: Rectangle class with print method

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+ * Ractangle class definition
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
