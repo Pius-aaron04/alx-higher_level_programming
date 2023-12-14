5607938771e7e81388617d97ce46bb95f41d1c48
Aaron Pius <piuschbz@gmail.com>
2023-12-14 03:26:17 +0000

    update: occurrence function

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function nbOccurences (list, searchElement) {
+  let occurrences = 0;
+  list.forEach((element) => {
+    if (element === searchElement) {
+	    occurrences += 1;
+    }
+  })
+  return occurrences;
+}
+
+module.exports = { nbOccurences };
