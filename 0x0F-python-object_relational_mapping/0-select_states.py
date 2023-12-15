230b12f34bcccc77fdb4798a482004878272a821
Aaron Pius <piuschbz@gmail.com>
2023-12-15 10:54:50 +0000

    update: execution on importation voided

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,18 +6,17 @@ Accesses lists of states in database
 import sys
 import MySQLdb
 
+if __name__ == '__main__':
+    argv = sys.argv[1:]
+    username, password, database = argv
 
-argv = sys.argv[1:]
-
-username, password, database = argv
-
-db = MySQLdb.connect(host='localhost', user=username, passwd=password,
+    db = MySQLdb.connect(host='localhost', user=username, passwd=password,
                      db=database)
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-data = cur.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    data = cur.fetchall()
 
-for value in data:
-    print(value)
-cur.close()
-db.close()
+    for value in data:
+        print(value)
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,19 +6,19 @@ Accesses lists of states in database
 import sys
 import MySQLdb
 
+if __name__ == '__main__':
+    argv = sys.argv[1:]
 
-argv = sys.argv[1:]
+    username, password, database = argv
 
-username, password, database = argv
+    db = MySQLdb.connect(host='localhost', user=username, passwd=password,
+                         db=database, port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    data = cur.fetchall()
 
-db = MySQLdb.connect(host='localhost', user=username, passwd=password,
-                     db=database, port=3306)
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id")
-data = cur.fetchall()
-
-for value in data:
-    if value[1].startswith('N'):
-        print(value)
-cur.close()
-db.close()
+    for value in data:
+        if value[1].startswith('N'):
+            print(value)
+    cur.close()
+    db.close()
