e2fd2e70fcc26878e9c2c90e6fd8346c7938b666
Aaron Pius <piuschbz@gmail.com>
2023-12-16 10:33:11 +0000

    add: get state script

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" lists the first state object."""
+
+if __name__ == '__main__':
+    import sys
+    from model_state import Base, State
+    import sqlalchemy
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    conn_string = 'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
+                                                              sys.argv[2],
+                                                              sys.argv[3])
+    engine = create_engine(conn_string, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).\
+        filter(State.name.in_([sys.argv[4]])).first()
+
+    if state:
+        print(state.id)
+    else:
+        print('Not found')
