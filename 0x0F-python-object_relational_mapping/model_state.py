ec958fd94a7abe116d61ec0225cbf1d707b23ad6
Aaron Pius <piuschbz@gmail.com>
2023-12-16 07:53:53 +0000

    add: docs for module and State link class

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-
+"""
+defines the states database table link class
+"""
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer
 
+
 Base = declarative_base()
 
+
 class State(Base):
+    """states table link class."""
+
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement='auto')
     name = Column(String(128), nullable=False)
