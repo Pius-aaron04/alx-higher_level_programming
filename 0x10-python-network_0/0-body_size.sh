fdb99ffe468f1efeede49a7b66f77315fc774e4f
Pius Aaron <piuschbz@gmail.com>
2024-01-07 14:58:18 +0100

    Checking out requirement fail

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/bash
 # Sends request to the URL for body content
 curl -s -o /dev/null -w "%{size_download}\n" "$1"
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 # dislays the content in a url if it's succuessfull
 status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1")
 
