091ed232f803dcc2b69631a99eb4424f9cfeceb9
Pius Aaron <piuschbz@gmail.com>
2024-01-08 17:32:25 +0100

    print str instead of bytes

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,4 +13,4 @@ if __name__ == '__main__':
     req = urllib.request.Request(sys.argv[1], data)
 
     with urllib.request.urlopen(req) as response:
-        print(response.read())
+        print(response.read().decode('utf-8'))
