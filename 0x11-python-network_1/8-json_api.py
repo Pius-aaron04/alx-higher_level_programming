a5218e3c8872a43850b52e5da0092a381088e471
Pius Aaron <piuschbz@gmail.com>
2024-01-08 03:22:50 +0100

    I wan sleep abeg

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,5 +6,6 @@ sends a post request to a sever.
 import requests
 import sys
 
-r = requests.post('http://0.0.0.0:5000/search_user', data={sys.argv[1]: ""})
-print(r.text)
+if __name__ == '__main__':
+    r = requests.post('http://0.0.0.0:5000/search_user', data={sys.argv[1]: ""})
+    print(r.text)
